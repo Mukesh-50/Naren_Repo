8972a6e6f404545faf80e2af22bf222edde2eefe
Mukesh_50 <Mukesh_50@Mukesh>
2016-03-11 09:35:24 +0530

    Test cases added
    Signed-off-by: Mukesh_50 <Mukesh_50@Mukesh>

---
+++ src/test/java/Regression/VerifyTitle.java
@@ -0,0 +1,18 @@
+package Regression;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+	
+	@Test
+	public void test123()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.learn-automation.com");
+		Assert.assertTrue(driver.getTitle().contains("Selenium"));
+	}
+
+}
