dad5d32924408d14ba1c37351ce85d6be38d32e9
Mukesh-50 <mukeshotwani.50@gmail.com>
2016-03-11 09:37:00 +0530

    Update VerifyTitle.java

--- src/test/java/Regression/VerifyTitle.java
+++ src/test/java/Regression/VerifyTitle.java
@@ -10,6 +10,7 @@ public class VerifyTitle {
 	@Test
 	public void test123()
 	{
+		// Naren changes
 		WebDriver driver=new FirefoxDriver();
 		driver.get("http://www.learn-automation.com");
 		Assert.assertTrue(driver.getTitle().contains("Selenium"));
